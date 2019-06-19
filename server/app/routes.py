457345b11c5edc92ec3c31aba8b2a4e2fae09f69
Anson VanDoren <anson.vandoren@gmail.com>
2019-06-18 20:56:10 -0700

    basic read/write

--- server/app/routes.py
+++ server/app/routes.py
@@ -1,7 +1,33 @@
+from flask import abort, jsonify, render_template, request
 from app import app
+from app.store import CSVStore
 
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "Smart home stuff"
+    """
+    show the main page
+    """
+    office_sensor = CSVStore.get_last_reading("office")
+    return render_template("index.html", sensors=[office_sensor])
+
+
+@app.route("/sensor/add_reading", methods=["POST"])
+def update_from_sensor():
+    if (
+        not request.json
+        or "sensor_name" not in request.json
+        or "sensor_id" not in request.json
+    ):
+        abort(400)
+
+    sensor_name = request.json["sensor_name"]
+    sensor_id = request.json["sensor_id"]
+    temp = request.json.get("temperature", None)
+    humidity = request.json.get("humidity", None)
+
+    CSVStore.add_sensor_reading(sensor_name, temp, humidity)
+
+    sensor_name = request.json["sensor_name"]
+    return jsonify({"sensor_name": sensor_name}), 200
---
+++ server/app/store.py
@@ -0,0 +1,30 @@
+import csv
+import datetime
+
+CSV_FILE = "./readings.csv"
+
+
+class CSVStore:
+    filename = CSV_FILE
+
+    @classmethod
+    def add_sensor_reading(cls, sensor_name, temperature, humidity):
+        with open(cls.filename, "a") as csv_file:
+            writer = csv.writer(csv_file, delimiter=",")
+            timestamp = datetime.datetime.utcnow().strftime(r"%Y-%m-%d %H:%M:%S")
+            writer.writerow([timestamp, sensor_name, temperature, humidity])
+
+    @classmethod
+    def get_last_reading(cls, desired_sensor_name):
+        with open(cls.filename, "r") as csv_file:
+            for row in csv_file.readlines()[-2:]:
+                timestamp, sensor_name, temperature, humidity = row.split(",")
+                if sensor_name == desired_sensor_name:
+                    return {
+                        "location": sensor_name,
+                        "timestamp": timestamp,
+                        "temperature": temperature,
+                        "humidity": humidity,
+                    }
+            else:
+                return None
