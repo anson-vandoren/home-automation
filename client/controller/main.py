3c2057d9018715e02c97682ce5f519897e507f61
Anson VanDoren <anson.vandoren@gmail.com>
2019-06-24 20:50:34 -0700

    UI development

--- client/controller/main.py
+++ client/controller/main.py
@@ -1,18 +1,162 @@
-import time
+import utime
+import ssd1306
+import machine
 from rotary import Rotary
+from config import *
 
-PIN_CLK = 14  # D5
-PIN_DT = 13  # D7
+import roboto16 as small_font
+import roboto48 as large_font
+import writer
 
-r = Rotary(pin_clk=PIN_CLK, pin_dt=PIN_DT, min_val=50, max_val=90)
 
-lastval = r.value()
+_WIDTH = 128
+
+# set up rotary encoder
+r_temp = Rotary(
+    pin_clk=PIN_CLK,
+    pin_dt=PIN_DT,
+    min_val=50,
+    max_val=90,
+    range_mode=Rotary.RANGE_BOUNDED,
+    enabled=False,
+)
+r_menu = Rotary(
+    pin_clk=PIN_CLK,
+    pin_dt=PIN_DT,
+    min_val=0,
+    max_val=3,
+    range_mode=Rotary.RANGE_WRAP,
+    enabled=True,
+)
+
+
+i2c = machine.I2C(scl=machine.Pin(DISPLAY_SCL), sda=machine.Pin(DISPLAY_SDA))
+display = ssd1306.SSD1306_I2C(128, 64, i2c)
+# set display to partial brightness
+disp_contrast = 100
+display.contrast(disp_contrast)
+
+small_font_writer = writer.Writer(display, small_font)
+large_font_writer = writer.Writer(display, large_font)
+
+lastval = r_temp.value()
+
+is_menu = True
+curr_sel = 1
+r_menu.value(curr_sel)
+
+
+_SEL_MENU = 0
+_SEL_SET = 1
+_MENU_WIDTH = 32
+
+last_sw_click_millis = utime.ticks_ms()
+
+
+def rotary_switch(pin):
+    global is_menu, last_sw_click_millis
+    # debounce switch click
+    if utime.ticks_diff(utime.ticks_ms(), last_sw_click_millis) > 100:
+        is_menu = not is_menu
+        last_sw_click_millis = utime.ticks_ms()
+        if r_temp.is_enabled:
+            # switch to menu
+            r_temp.disable()
+            r_menu.enable()
+
+            clear_control_area()
+            draw_menu_box(color=1)
+
+            display.show()
+        else:
+            # switch to control
+            r_menu.disable()
+            r_temp.enable()
+
+            draw_menu_box(color=0)
+            write_temp(r_temp.value())
+            draw_control_box(color=1)
+
+            display.show()
+
+
+# set up rotary encoder switch
+r_sw = machine.Pin(PIN_ROT_SW, machine.Pin.IN)
+r_sw.irq(trigger=machine.Pin.IRQ_RISING, handler=rotary_switch)
+
+
+def write_temp(val):
+
+    temp_str = str(val) + "°"
+    temp_str_len = large_font_writer.stringlen(temp_str)
+
+    temp_str_left = _MENU_WIDTH + (_WIDTH - temp_str_len - _MENU_WIDTH) // 2
+    # the degree character renders too wide in chosen font. remove some rpad
+    temp_str_left += 4
+
+    large_font_writer.set_textpos(temp_str_left, 8)
+    large_font_writer.printstring(temp_str)
+
+
+def draw_menu_box(color=1):
+    top = curr_sel * 16
+    bottom = top + 15
+
+    display.hline(0, top, _MENU_WIDTH, color)
+    display.hline(0, bottom, _MENU_WIDTH, color)
+    display.vline(0, top, 16, color)
+    display.vline(_MENU_WIDTH, top, 16, color)
+
+
+def draw_control_box(color=1):
+    display.rect(_MENU_WIDTH, 0, (128 - _MENU_WIDTH), 64, color)
+
+
+def clear_control_area():
+    display.fill_rect(_MENU_WIDTH, 0, (128 - _MENU_WIDTH), 64, 0)
+
+
+def write_menu():
+    print("writing menu text")
+    display.fill_rect(0, 0, _MENU_WIDTH, 64, 0)
+
+    # set menu
+    labels = ["SET", "75F", "HUM", "45%"]
+    for i, menu_label in enumerate(labels):
+        str_len = small_font_writer.stringlen(menu_label)
+        str_left = 1 + (_MENU_WIDTH - str_len) // 2
+        small_font_writer.set_textpos(str_left, i * 16)
+        small_font_writer.printstring(menu_label)
+
+    # draw selected element
+    draw_menu_box(color=1)
+
+
+write_menu()
+display.show()
+
 while True:
-    val = r.value()
+    if r_temp.is_enabled:
+        val = r_temp.value()
+        if val != lastval:
+            lastval = val
+            print("updating control")
 
-    if lastval != val:
-        lastval = val
+            write_temp(val)
+            draw_control_box(color=1)
+            display.show()
 
-        print("result =", val)
+    elif r_menu.is_enabled:
+        new_menu_val = r_menu.value()
+        if curr_sel != new_menu_val:
+            print("updating menu")
+            # remove old selection box
+            draw_menu_box(color=0)
+            # update to new selection
+            curr_sel = new_menu_val
+            # draw new selection box
+            draw_menu_box(color=1)
+            # flip display buffer
+            display.show()
 
-    time.sleep_ms(50)
+    utime.sleep_ms(50)
--- client/controller/rotary.py
+++ client/controller/rotary.py
@@ -11,28 +11,66 @@ _R_CCW_1 = const(0x4)
 _R_CCW_2 = const(0x5)
 _R_CCW_3 = const(0x6)
 
+# _t_table = [
+#     [_R_START, _R_CCW_1, _R_CW_1, _R_START],
+#     [_R_START, _R_START, _R_CW_1, _R_CW_2],
+#     [_R_START, _R_CW_3, _R_CW_1, _R_CW_2],
+#     [_DIR_CW, _R_CW_3, _R_START, _R_CW_2],
+#     [_R_START, _R_CCW_1, _R_START, _R_CCW_2],
+#     [_R_START, _R_CCW_1, _R_CCW_3, _R_CCW_2],
+#     [_DIR_CCW, _R_START, _R_CCW_3, _R_CCW_2],
+# ]
+
 _t_table = [
     [_R_START, _R_CCW_1, _R_CW_1, _R_START],
-    [_R_START, _R_START, _R_CW_1, _R_CW_2],
-    [_R_START, _R_CW_3, _R_CW_1, _R_CW_2],
-    [_DIR_CW, _R_CW_3, _R_START, _R_CW_2],
-    [_R_START, _R_CCW_1, _R_START, _R_CCW_2],
-    [_R_START, _R_CCW_1, _R_CCW_3, _R_CCW_2],
-    [_DIR_CCW, _R_START, _R_CCW_3, _R_CCW_2],
+    [_R_CW_2, _R_START, _R_CW_1, _R_START],
+    [_R_CW_2, _R_CW_3, _R_CW_1, _R_START],
+    [_R_CW_2, _R_CW_3, _R_START, _DIR_CCW],
+    [_R_CCW_2, _R_CCW_1, _R_START, _R_START],
+    [_R_CCW_2, _R_CCW_1, _R_CCW_3, _R_START],
+    [_R_CCW_2, _R_START, _R_CCW_3, _DIR_CW],
 ]
 
 _STATE_MASK = const(0x7)
 _DIR_MASK = const(0x30)
 
 
+def _bound(value, incr, lower_bound, upper_bound):
+    return min(upper_bound, max(lower_bound, value + incr))
+
+
+def _wrap(value, incr, lower_bound, upper_bound):
+    range = upper_bound - lower_bound + 1
+    value = value + incr
+
+    if value < lower_bound:
+        value += range * ((lower_bound - value) // range + 1)
+
+    return lower_bound + (value - lower_bound) % range
+
+
 class Rotary(object):
-    def __init__(self, pin_clk, pin_dt, min_val, max_val):
+    RANGE_BOUNDED = const(1)
+    RANGE_WRAP = const(2)
+
+    def __init__(self, pin_clk, pin_dt, min_val, max_val, range_mode, enabled):
         self._min_val = min_val
         self._max_val = max_val
+        self._range_mode = range_mode
         # set up pins
-        self._pin_clk = Pin(pin_clk, Pin.IN)
-        self._pin_dt = Pin(pin_dt, Pin.IN)
+        self._pin_clk = Pin(pin_clk, Pin.IN, Pin.PULL_UP)
+        self._pin_dt = Pin(pin_dt, Pin.IN, Pin.PULL_UP)
 
+        # set initial state
+        self._state = _R_START
+        self._value = min_val
+
+        if enabled:
+            self.enable()
+        else:
+            self.is_enabled = False
+
+    def enable(self):
         # enable IRQs
         self._pin_clk.irq(
             trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=self._process_trigger
@@ -40,27 +78,39 @@ class Rotary(object):
         self._pin_dt.irq(
             trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=self._process_trigger
         )
+        self.is_enabled = True
 
-        # set initial state
-        self._state = _R_START
-        self._value = min_val
+    def disable(self):
+        self._pin_clk.irq(handler=None)
+        self._pin_dt.irq(handler=None)
+        self.is_enabled = False
 
-    def value(self):
-        return self._value
+    def value(self, new_val=None):
+        if new_val is None:
+            return self._value
+        else:
+            self._value = new_val
 
     def _process_trigger(self, pin):
         # get the updated pin values
         next_pins = (self._pin_clk.value() << 1) | self._pin_dt.value()
+
         # if last state was a directional turn, reset, otherwise leave unchanged
         self._state &= _STATE_MASK
+
         # get next state from the transition table
         self._state = _t_table[self._state][next_pins]
+
         # check if a full 'click' is achieved
         direction = self._state & _DIR_MASK
         # convert direction bits to +/- 1
-        incr = -2 * ((0x1) & ((direction >> 4) ^ (0x3))) + 1
-        # update value if a full click
-        self._value += incr if direction else 0
-        # bound value at max and min
-        self._value = min(self._max_val, max(self._min_val, self._value))
+        incr = -2 * ((0x1) & ((direction >> 4) ^ (0x3))) + 1 if direction else 0
 
+        if not incr:
+            return
+
+        # bound value at max and min
+        if self._range_mode == self.RANGE_BOUNDED:
+            self._value = _bound(self._value, incr, self._min_val, self._max_val)
+        elif self._range_mode == self.RANGE_WRAP:
+            self._value = _wrap(self._value, incr, self._min_val, self._max_val)
